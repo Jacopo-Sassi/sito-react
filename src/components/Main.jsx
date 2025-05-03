0a3abbd71fffd4a5a6fd1b5843c827d2873885e8
Jacopo Sassi <112653681+Jacopo-Sassi@users.noreply.github.com>
2025-05-03 22:53:42 +0200

    added main title and the first main paragraph

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,21 @@
 function Main() {
   return (
     <div className="main">
-      <h1>Welcome to the Main Component</h1>
-      <p>This is the main content area.</p>
+      <div className="main-card">
+      <img src="mail.svg" alt="Mail" className="mail-icon"/>
+      <p className="main-card-text">No more emails, calls or waiting around</p>
+      </div>
+      <h1 className="main-title">Take control of the process with FSA</h1>
+      <div className="main-description">
+        <div className="main-description-text">
+        <p className="subtitle-main-description">1. Step-by-step-guidance</p>
+        <h1 className="main-description-title">Insider experience</h1>
+        <p className="main-description-inner-text">FSA is built by experts in college recruitment. We’ve been in the game for years, and we’ve distilled our deep experience to craft a process that works for athletes and coaches alike.</p>
+        </div>
+        <div className="main-description-img">
+          <img src="example.jpg" alt="example" className="main-description-image"/>
+        </div>
+      </div>
     </div>
   );
 }
